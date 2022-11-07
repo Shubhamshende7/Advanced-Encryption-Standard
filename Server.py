9d9095bee49e0f980ad07c6191e77480c39859f3
Shubham Shende <69889323+Shubhamshende7@users.noreply.github.com>
2022-11-07 21:40:42 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,24 @@
 import socket
 
-HOST = '192.168.30.20'    # The remote host
-PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+if __name__ == "__main__":
+    ip = "192.168.30.10"
+    port = 8080
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+
+    server.listen(10)
+
+    while True:
+        client, address = server.accept()
+        print(f"Connection Established: {address[0]}:{address[1]}")
+
+        client.send(bytes("Who are you?", "utf-8"))
+        
+        data = client.recv(1024)
+        data = data.decode("utf-8")
+        print(data)
+
+        client.close()
+
+    
